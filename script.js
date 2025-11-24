8228b50e03697f79a2e3a99b906ed4f52e96f979
leilailu <107797935+leilailu@users.noreply.github.com>
2025-11-24 19:40:58 +0100

    Update script.js
    debug sier vi

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+console.log("JavaScript koblet til!");
+
+document.getElementById("helloBtn").addEventListener("click", function() {
+    document.getElementById("message").textContent = "Hei! Du trykket på knappen";
+});
 
