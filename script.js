eed1feff9fda0a2566fe7130270bf94786686530
leilailu <107797935+leilailu@users.noreply.github.com>
2025-11-24 20:17:06 +0100

    Update script.js
    js v2

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-console.log("JavaScript koblet til!");
-
-document.getElementById("helloBtn").addEventListener("click", function() {
-    document.getElementById("message").textContent = "Hei! Du trykket på knappen";
+// Smooth scroll knapp
+document.getElementById("scrollButton").addEventListener("click", () => {
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
 });
 
+// Log for å vise at JS funker (forhåpentligvis)
+console.log("Portfolio JS lastet inn!");
